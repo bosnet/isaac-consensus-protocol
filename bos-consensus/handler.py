d84a507e39d6f06a070e224d547288e765fd30c1
Spike^ekipS <spikeekips@gmail.com>
2017-12-06 17:23:28 +0900

    The basic node configuration will be refered from `BOSNetHTTPServer`.

--- bos-consensus/handler.py
+++ bos-consensus/handler.py
@@ -4,34 +4,34 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def not_found_handler(server, parsed):
+def not_found_handler(handler, parsed):
     if parsed.path[1:] == '':
-        return handle_ping(server, parsed)
+        return handle_ping(handler, parsed)
 
     message = '"%s" not found' % parsed.path
-    server.response(404, message)
+    handler.response(404, message)
 
     return
 
 
-def handle_ping(server, parsed):
-    if server.command not in ('POST',):
-        server.response(405, None)
+def handle_ping(handler, parsed):
+    if handler.command not in ('POST',):
+        handler.response(405, None)
         return
 
-    # length = int(server.headers['Content-Length'])
-    # post_data = server.rfile.read(length).decode('utf-8')
+    # length = int(handler.headers['Content-Length'])
+    # post_data = handler.rfile.read(length).decode('utf-8')
 
     # d = json.loads(post_data)
-    # host, _ = server.request.getpeername()
+    # host, _ = handler.request.getpeername()
     # endpoint = 'http://%s:%s' % (host, d['port'])
 
-    host, port = server.request.getsockname()
     info = dict(
-        node_id=server.node_id,
-        endpoint='http://%s:%s' % (host, port)
+        node_id=handler.server.node_id,
+        validators=handler.server.validators,
+        endpoint=handler.server.endpoint,
     )
-    server.json_response(200, info)
+    handler.json_response(200, info)
 
     return
 
--- bos-consensus/network.py
+++ bos-consensus/network.py
@@ -10,30 +10,31 @@ log = logging.getLogger(__name__)
 
 
 class BOSNetHTTPServer(HTTPServer):
+    host = None
+    port = None
     node_id = None
     validators = None
 
-    def __init__(self, node_id, validators, *a, **kw):
-        super(BOSNetHTTPServer, self).__init__(*a, **kw)
+    def __init__(self, node_id, validators, address, *a, **kw):
+        super(BOSNetHTTPServer, self).__init__(address, *a, **kw)
 
         self.node_id = node_id
         self.validators = validators
+        self.host, self.port = tuple(address)
 
     def finish_request(self, request, client_address):
-        self.RequestHandlerClass(self.node_id, self.validators, request, client_address, self)
+        self.RequestHandlerClass(request, client_address, self)
 
         return
 
+    @property
+    def endpoint(self):
+        return 'http://%s:%s' % (self.host, self.port)
+
 
 class BOSNetHTTPServerRequestHandler(BaseHTTPRequestHandler):
-    node_id = None
-    validators = None
-
-    def __init__(self, node_id, validators, *a, **kw):
+    def __init__(self, *a, **kw):
         super(BOSNetHTTPServerRequestHandler, self).__init__(*a, **kw)
-
-        self.node_id = node_id
-        self.validators = validators
 
     def do_GET(self):
         log.debug('> start request: %s', self.path)
