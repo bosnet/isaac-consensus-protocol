5aadde9c4f7dd712aa71ec7e3e991419b9b0ae4f
Spike^ekipS <spikeekips@gmail.com>
2017-12-06 16:09:26 +0900

    cleanup code

--- bos-consensus/run_node.py
+++ bos-consensus/run_node.py
@@ -45,16 +45,18 @@ if __name__ == '__main__':
     )(uuid.uuid1().hex, 8001, [])
 
     if len(sys.argv) != 2:
-        error_message = 'usage: ' + __file__ + ' conf.ini '
+        error_message = 'usage: %s <config ini>' % (sys.argv[0],)
         print(error_message)
         log.error(error_message)
         exit(2)
+
     input_ini_path = sys.argv[1].strip('"\'')
     if not pathlib.Path(input_ini_path).is_file():
         error_message = 'File "' + input_ini_path + '" not exists!'
         print(error_message)
         log.error(error_message)
         exit(2)
+
     log.info('Ini file path: "' + input_ini_path + '"')
     log.root.setLevel(logging.DEBUG)
     conf.read(input_ini_path)
@@ -67,6 +69,7 @@ if __name__ == '__main__':
     validator_list = []
     for i in filter(lambda x: len(x.strip()) > 0, conf['NODE']['VALIDATOR_LIST'].split(',')):
         validator_list.append(i.strip())
+
     options = options._replace(validators=validator_list)
     log.debug('Validators: %s' % options.validators)
 
