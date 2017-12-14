7403b5111c951a18bc9142e0d93c925d4cb0c7f2
charles <charleslee522@gmail.com>
2017-12-14 12:34:08 +0900

    가독성을 위해 밸롯 생성 시 명시적인 kind를 전달하도록 수정.

--- bos-consensus/tests/test_node_state.py
+++ bos-consensus/tests/test_node_state.py
@@ -25,14 +25,14 @@ def test_state_init_to_sign():
 
     node1.init_node()
 
-    ballot = Ballot(1, 1, 'message', node1.node_state.kind)
+    ballot = Ballot(1, 1, 'message', StateKind.INIT)
 
     node1.receive(ballot)
 
     assert node1.node_state.kind == StateKind.SIGN
 
 
-def test_state_init_to_all_confirm():
+def test_state_init_to_all_confirm_sequence():
     node1 = Node(1, ('localhost', 5001), 100, ['localhost:5002', 'localhost:5003'])
     node2 = Node(2, ('localhost', 5002), 100, ['localhost:5001', 'localhost:5003'])
     node3 = Node(3, ('localhost', 5003), 100, ['localhost:5001', 'localhost:5002'])
@@ -41,7 +41,7 @@ def test_state_init_to_all_confirm():
     node2.init_node()
     node3.init_node()
 
-    ballot_init_1 = Ballot(1, 1, 'message', node1.node_state.kind)
+    ballot_init_1 = Ballot(1, 1, 'message', StateKind.INIT)
 
     node1.receive(ballot_init_1)
     node2.receive(ballot_init_1)
@@ -51,9 +51,9 @@ def test_state_init_to_all_confirm():
     assert isinstance(node2.node_state, SignState)
     assert isinstance(node3.node_state, SignState)
 
-    ballot_sign_1 = Ballot(1, 1, 'message', node1.node_state.kind)
-    ballot_sign_2 = Ballot(1, 2, 'message', node2.node_state.kind)
-    ballot_sign_3 = Ballot(1, 3, 'message', node3.node_state.kind)
+    ballot_sign_1 = Ballot(1, 1, 'message', StateKind.SIGN)
+    ballot_sign_2 = Ballot(1, 2, 'message', StateKind.SIGN)
+    ballot_sign_3 = Ballot(1, 3, 'message', StateKind.SIGN)
 
     node1.receive(ballot_sign_1)
     node2.receive(ballot_sign_1)
@@ -71,9 +71,9 @@ def test_state_init_to_all_confirm():
     assert isinstance(node2.node_state, AcceptState)
     assert isinstance(node3.node_state, AcceptState)
 
-    ballot_accept_1 = Ballot(1, 1, 'message', node1.node_state.kind)
-    ballot_accept_2 = Ballot(1, 2, 'message', node2.node_state.kind)
-    ballot_accept_3 = Ballot(1, 3, 'message', node3.node_state.kind)
+    ballot_accept_1 = Ballot(1, 1, 'message', StateKind.ACCEPT)
+    ballot_accept_2 = Ballot(1, 2, 'message', StateKind.ACCEPT)
+    ballot_accept_3 = Ballot(1, 3, 'message', StateKind.ACCEPT)
 
     node1.receive(ballot_accept_1)
     node1.receive(ballot_sign_1)    # different state ballot
@@ -91,3 +91,64 @@ def test_state_init_to_all_confirm():
     assert isinstance(node1.node_state, AllConfirmState)
     assert isinstance(node2.node_state, AcceptState)
     assert isinstance(node3.node_state, AllConfirmState)
+
+
+# def test_state_init_to_state_jump():
+#     node1 = Node(1, ('localhost', 5001), 100, ['localhost:5002', 'localhost:5003'])
+#     node2 = Node(2, ('localhost', 5002), 100, ['localhost:5001', 'localhost:5003'])
+#     node3 = Node(3, ('localhost', 5003), 100, ['localhost:5001', 'localhost:5002'])
+
+#     node1.init_node()
+#     node2.init_node()
+#     node3.init_node()
+
+#     ballot_init_1 = Ballot(1, 1, 'message', StateKind.INIT)
+
+#     node1.receive(ballot_init_1)
+#     node2.receive(ballot_init_1)
+#     node3.receive(ballot_init_1)
+
+#     assert isinstance(node1.node_state, SignState)
+#     assert isinstance(node2.node_state, SignState)
+#     assert isinstance(node3.node_state, SignState)
+
+#     ballot_sign_1 = Ballot(1, 1, 'message', StateKind.SIGN)
+#     ballot_sign_2 = Ballot(1, 2, 'message', StateKind.ACCEPT)
+#     ballot_sign_3 = Ballot(1, 3, 'message', StateKind.SIGN)
+
+#     node1.receive(ballot_sign_1)
+#     node2.receive(ballot_sign_1)
+#     node3.receive(ballot_sign_1)
+
+#     node1.receive(ballot_sign_2)
+#     node2.receive(ballot_sign_2)
+#     node3.receive(ballot_sign_2)
+
+#     node1.receive(ballot_sign_3)
+#     node2.receive(ballot_sign_3)
+#     node3.receive(ballot_sign_3)
+
+#     assert isinstance(node1.node_state, AcceptState)
+#     assert isinstance(node2.node_state, AcceptState)
+#     assert isinstance(node3.node_state, AcceptState)
+
+#     ballot_accept_1 = Ballot(1, 1, 'message', StateKind.ACCEPT)
+#     ballot_accept_2 = Ballot(1, 2, 'message', StateKind.ACCEPT)
+#     ballot_accept_3 = Ballot(1, 3, 'message', StateKind.ACCEPT)
+
+#     node1.receive(ballot_accept_1)
+#     node1.receive(ballot_sign_1)    # different state ballot
+#     node2.receive(ballot_accept_1)
+#     node3.receive(ballot_accept_1)
+
+#     node1.receive(ballot_accept_2)
+#     node2.receive(ballot_accept_2)
+#     node3.receive(ballot_accept_2)
+
+#     node1.receive(ballot_accept_3)
+#     node2.receive(ballot_sign_3)    # different state ballot
+#     node3.receive(ballot_accept_3)
+
+#     assert isinstance(node1.node_state, AllConfirmState)
+#     assert isinstance(node2.node_state, AcceptState)
+#     assert isinstance(node3.node_state, AllConfirmState)
