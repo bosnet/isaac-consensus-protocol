259299e03eb9d275479ddab5a23129d327e6a032
charles <charleslee522@gmail.com>
2017-12-18 09:41:28 +0900

    서버-클라이언트 방식으로 프로토콜 쿼리 테스트 작성

---
+++ tests/test_network_interface.py
@@ -0,0 +1,217 @@
+from bos_consensus.ballot import Ballot
+from bos_consensus.statekind import StateKind
+from bos_consensus.node import Node
+from bos_consensus.network import (
+    BOSNetHTTPServer,
+    BOSNetHTTPServerRequestHandler,
+)
+
+import json
+import requests
+import threading
+import urllib
+
+
+def test_sample():
+    print('test_sample')
+    assert 1 == 1
+
+
+class Server(threading.Thread):
+    def __init__(self, port, node_id):
+        super(Server, self).__init__()
+        self.port = port
+        self.node_id = node_id
+
+    def run(self):
+        node = Node(self.node_id, ('localhost', self.port), 100, ['localhost:5002', 'localhost:5003'])
+        httpd = BOSNetHTTPServer(node, ('localhost', self.port), BOSNetHTTPServerRequestHandler)
+        httpd.serve_forever()
+        return True
+
+
+class Client(threading.Thread):
+    def __init__(self, port):
+        super(Client, self).__init__()
+        self.port = port
+        self.response_code = 0
+        self.url_format = 'http://localhost:%d'
+
+    def run(self):
+        self.run_impl()
+        return
+
+    def run_impl(self):
+        raise NotImplementedError()
+
+    def get_response_code(self):
+        return self.response_code
+
+
+class Ping(Client):
+    def __init__(self, port):
+        super(Ping, self).__init__(port)
+
+    def run_impl(self):
+        try:
+            response = requests.get(
+                urllib.parse.urljoin(self.url_format % self.port, '/ping')
+                )
+
+            self.response_code = response.status_code
+        except requests.exceptions.ConnectionError:
+            print('Connection Refused!')
+        return True
+
+
+def test_handler_ping():
+    port = 5001
+    server_thread = Server(port, 5001)
+    server_thread.daemon = True
+    server_thread.start()
+
+    client_ping_thread = Ping(port)
+    client_ping_thread.daemon = True
+    client_ping_thread.start()
+    client_ping_thread.join()
+    assert client_ping_thread.get_response_code() == 200
+
+
+class Status(Client):
+    def __init__(self, port):
+        super(Status, self).__init__(port)
+        self.node_id = 0
+
+    def run_impl(self):
+        try:
+            response = requests.get(
+                urllib.parse.urljoin(self.url_format % self.port, '/status')
+                )
+            self.response_code = response.status_code
+            self.node_id = json.loads(response.text)['Node']['node_id']
+        except requests.exceptions.ConnectionError:
+            print('Connection Refused!')
+        return True
+
+    def get_node_id(self):
+        return self.node_id
+
+
+def test_handler_status():
+    port = 5001
+    node_id = 5001
+    server_thread = Server(port, node_id)
+    server_thread.daemon = True
+    server_thread.start()
+
+    client_ping_thread = Status(port)
+    client_ping_thread.daemon = True
+    client_ping_thread.start()
+    client_ping_thread.join()
+    assert client_ping_thread.get_response_code() == 200
+    assert client_ping_thread.get_node_id() == node_id
+
+
+class GetNode(Client):
+    def __init__(self, port):
+        super(GetNode, self).__init__(port)
+        self.node_id = 0
+
+    def run_impl(self):
+        try:
+            url = 'http://localhost:%d' % self.port
+            response = requests.get(
+                urllib.parse.urljoin(url, '/get_node')
+                )
+
+            self.response_code = response.status_code
+            self.node_id = json.loads(response.text)['node_id']
+
+        except requests.exceptions.ConnectionError:
+            print('Connection Refused!')
+        return True
+
+    def get_node_id(self):
+        return self.node_id
+
+def test_handler_get_node():
+    port = 5001
+    node_id = 5001
+    server_thread = Server(port, node_id)
+    server_thread.daemon = True
+    server_thread.start()
+
+    client_ping_thread = GetNode(port)
+    client_ping_thread.daemon = True
+    client_ping_thread.start()
+    client_ping_thread.join()
+    assert client_ping_thread.get_response_code() == 200
+    assert client_ping_thread.get_node_id() == node_id
+
+
+class SendMessage(Client):
+    def __init__(self, port, message):
+        super(SendMessage, self).__init__(port)
+        self.message = message
+
+    def run_impl(self):
+        try:
+            url = 'http://localhost:%d' % self.port
+            post_data = json.dumps({'message': self.message})
+            response = requests.post(
+                urllib.parse.urljoin(url, '/send_message'),
+                data = post_data
+                )
+
+            self.response_code = response.status_code
+        except requests.exceptions.ConnectionError:
+            print('Connection Refused!')
+        return True
+
+
+def test_handler_send_message():
+    port = 5001
+    server_thread = Server(port, 5001)
+    server_thread.daemon = True
+    server_thread.start()
+
+    client_ping_thread = SendMessage(port, 'message')
+    client_ping_thread.daemon = True
+    client_ping_thread.start()
+    client_ping_thread.join()
+    assert client_ping_thread.get_response_code() == 200
+
+
+class SendBallot(Client):
+    def __init__(self, port, ballot):
+        super(SendBallot, self).__init__(port)
+        self.ballot = ballot
+
+    def run_impl(self):
+        try:
+            url = 'http://localhost:%d' % self.port
+
+            post_data = json.dumps(self.ballot.to_dict())
+            response = requests.post(
+                urllib.parse.urljoin(url, '/send_ballot'),
+                data = post_data
+                )
+
+            self.response_code = response.status_code
+        except requests.exceptions.ConnectionError:
+            print('Connection Refused!')
+        return True
+
+
+def test_handler_send_ballot():
+    port = 5001
+    server_thread = Server(port, 5001)
+    server_thread.daemon = True
+    server_thread.start()
+
+    ballot = Ballot(1, 5001, 'message', StateKind.INIT)
+    client_ping_thread = SendBallot(port, ballot)
+    client_ping_thread.daemon = True
+    client_ping_thread.start()
+    client_ping_thread.join()
+    assert client_ping_thread.get_response_code() == 200
