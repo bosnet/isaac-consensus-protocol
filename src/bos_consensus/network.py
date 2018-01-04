1c55d057fe6c139ead70768329f5142e7e371865
Spike^ekipS <spikeekips@gmail.com>
2018-01-04 09:47:16 +0900

    BOS-143; Ping will be disabled in `tests/test_network_interface.py`

--- src/bos_consensus/network.py
+++ src/bos_consensus/network.py
@@ -119,6 +119,8 @@ class BOSNetHTTPServer(ThreadingMixIn, HTTPServer):
         self.node = node
         self.node_manager = NodeManager(self.node)
 
+        self.start_ping()
+
     def start_ping(self):
         t = Ping(self.node)
         t.start()
