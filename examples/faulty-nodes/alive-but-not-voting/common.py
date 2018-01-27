42f5211c45fc56d79cff67de15a2c81fb85e75c1
Spike^ekipS <spikeekips@gmail.com>
2018-01-27 17:29:34 +0900

    BOS-164; the not voted nodes will be printed in metirc

---
+++ examples/faulty-nodes/alive-but-not-voting/common.py
@@ -0,0 +1,46 @@
+import random
+
+from bos_consensus.blockchain import Blockchain
+from bos_consensus.consensus import get_fba_module
+from bos_consensus.middlewares import (
+    BaseMiddleware,
+    StopConsensus,
+)
+
+
+consensus_module = get_fba_module('isaac')
+
+
+class SometimesNoVotingBlockchain(Blockchain):
+    faulty_frequency = None
+    no_voting_ballot_ids = None  # store the ballot to be skipped
+
+    def __init__(self, faulty_frequency, *a, **kw):
+        super(SometimesNoVotingBlockchain, self).__init__(*a, **kw)
+
+        self.middlewares.insert(0, SometimesNoVotingMiddleware)
+
+        assert type(faulty_frequency) in (float,)
+        assert faulty_frequency >= 0 and faulty_frequency <= 1.0
+
+        self.faulty_frequency = faulty_frequency
+        self.no_voting_ballot_ids = list()
+
+
+class SometimesNoVotingMiddleware(BaseMiddleware):
+    def received_ballot(self, ballot):
+        consensus = self.blockchain.consensus
+        if consensus.state in (consensus_module.IsaacState.INIT,):
+            if ballot.ballot_id not in self.blockchain.no_voting_ballot_ids and self.blockchain.faulty_frequency > 0.0:
+                if random.choices(range(10))[0] / 10 < self.blockchain.faulty_frequency:
+                    self.log.info('[%s] no voting for ballot, %s in %s', consensus.node.name, ballot, consensus.state)
+                    self.blockchain.no_voting_ballot_ids.append(ballot.ballot_id)
+
+                    raise StopConsensus()
+
+        if ballot.ballot_id in self.blockchain.no_voting_ballot_ids:
+            self.log.info('[%s] no voting for ballot, %s in %s', consensus.node.name, ballot, consensus.state)
+
+            raise StopConsensus()
+
+        return
